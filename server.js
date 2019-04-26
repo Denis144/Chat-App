aa48dc97562bde6c9b172f43a5a9f172c9e3667d
Denis144 <kdvv149@gmail.com>
2019-04-26 09:55:06 +0300

    updates heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Chat-App'));
+app.use(express.static(__dirname + '/dist/myChatApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/Chat-App/index.html'));
+res.sendFile(path.join(__dirname+'/dist/myChatApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
