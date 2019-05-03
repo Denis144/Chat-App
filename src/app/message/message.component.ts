5a238acd7c46215612b310e11577bf13f6e4f897
Denis144 <kdvv149@gmail.com>
2019-05-03 17:49:19 +0300

    New update

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ChatService } from '../services/chat.service';
-import { UsersService } from '../services/users.service';
+import { MessagesService } from '../services/messages.service';
 import { ChatMessage } from '../models/chat-message.model';
-import { EditMessageComponent } from '../edit-message/edit-message.component';
-import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-message',
@@ -13,59 +11,25 @@ import { MatDialog } from '@angular/material';
 })
 export class MessageComponent implements OnInit {
   @Input() chatMessage: ChatMessage;
-  currentUser: any;
   userName: string;
   messageContent: string;
   timeStamp: string;
-  message: Array<ChatMessage>;
-  flag = false;
+  isEdit = false;
 
-  constructor(private chat: ChatService, private usersService: UsersService, public dialog: MatDialog, private route: ActivatedRoute) {}
+  constructor(private chat: ChatService, private messagesService: MessagesService) {}
 
   ngOnInit(chatMessage = this.chatMessage) {
     this.messageContent = chatMessage.message;
     this.timeStamp = chatMessage.timeSend;
     this.userName = chatMessage.userName;
-    this.checkUser();
+    this.isEdit = this.messagesService.checkMess(this.chatMessage);
   }
 
-  checkUser() {
-    const observable = this.usersService.current();  
-    observable.subscribe(user => { this.currentUser = user; });
-    if (this.currentUser["userName"] === this.chatMessage.userName) {
-      this.flag = true;
-    } else {
-      this.flag = false;
-    }
+  deleted() {
+    this.messagesService.deleteMsg(this.chatMessage);
   }
 
-  deleteMsg() {
-    const obj = JSON.parse(localStorage.getItem('messages'));
-
-    for (const key in obj) {
-      if (obj[key]['timeSend'] === this.chatMessage.timeSend) {
-        obj.splice(key, 1);
-        localStorage.setItem('messages', JSON.stringify(obj));
-      }
-    }
-  }
-
-  editMsg() {
-    const dialogRef = this.dialog.open(EditMessageComponent, {
-      width: '400px',
-      data: {name: this.chatMessage.userName, time: this.chatMessage.timeSend, message: this.chatMessage.message }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      const obj = JSON.parse(localStorage.getItem('messages'));
-
-      obj.find(mess => {
-        if (mess.timeSend === this.chatMessage.timeSend) {
-            mess.message = result || this.chatMessage.message;
-          }
-      });
-
-      localStorage.setItem('messages', JSON.stringify(obj));
-    });
+  edited() {
+    this.messagesService.editMsg(this.chatMessage);
   }
 }
--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { ChatMessage } from '../models/chat-message.model';
+import { UsersService } from '../services/users.service';
+import { EditMessageComponent } from '../edit-message/edit-message.component';
+import { MatDialog } from '@angular/material';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagesService {
   subscriberMess = new BehaviorSubject<any>(new ChatMessage());
+  currentUser: any;
 
-  constructor() {}
+  constructor(private usersService: UsersService, public dialog: MatDialog) {}
 
   updateMessages(): void {
     const obj = JSON.parse(localStorage.getItem('messages'));
-    console.log(obj);
     this.subscriberMess.next(obj);
   }
 
@@ -20,5 +23,45 @@ export class MessagesService {
     this.updateMessages();
     return this.subscriberMess.asObservable();
   }
+
+  checkMess(mess) {
+    const observable = this.usersService.current();  
+    observable.subscribe(user => { this.currentUser = user; });
+    if (this.currentUser["userName"] === mess.userName) {
+      return true;
+    } 
+    return false;
+  }
+
+  deleteMsg(mess: ChatMessage) {
+    const obj = JSON.parse(localStorage.getItem('messages'));
+
+    for (const key in obj) {
+      if (obj[key]['timeSend'] === mess.timeSend) {
+        obj.splice(key, 1);
+        localStorage.setItem('messages', JSON.stringify(obj));
+      }
+    }
+  }
+
+  editMsg(messList) {
+    const dialogRef = this.dialog.open(EditMessageComponent, {
+      width: '400px',
+      data: {name: messList.userName, time: messList.timeSend, message: messList.message }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      const obj = JSON.parse(localStorage.getItem('messages'));
+
+      obj.find(mess => {
+        if (mess.timeSend === messList.timeSend) {
+          mess.message = result || messList.message;
+        }
+      });
+
+      localStorage.setItem('messages', JSON.stringify(obj));
+    });
+  }
+
 }
 // Сделать set и get localeStorage!!!
